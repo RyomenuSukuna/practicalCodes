006d4066409bf52cb0dca2e007660e80a7824e6d
Swanand Kulkarni <93972307+RyomenuSukuna@users.noreply.github.com>
2023-11-07 22:06:16 +0530

    Update implement live sequencing.py

--- implement live sequencing.py
+++ implement live sequencing.py
@@ -6,28 +6,23 @@ arr = [
     ['e', 1, 25]
 ]
 
-# Sort jobs by decreasing order of profit
 arr.sort(key=lambda x: x[2], reverse=True)
 
-# Total time slots available
 t = 3
 
-# To keep track of free time slots
 result = [False] * t
 
-# To store the result (Sequence of jobs)
 job = ['-1'] * t
 
-# Iterate through all given jobs
 for i in range(len(arr)):
-    # Find a free slot for this job
+
     for j in range(min(t - 1, arr[i][1] - 1), -1, -1):
-        # Free slot found
+      
         if result[j] is False:
             result[j] = True
             job[j] = arr[i][0]
             break
 
-# Print the sequence
+
 print("Following is the maximum profit sequence of Jobs: ")
 print(job)
