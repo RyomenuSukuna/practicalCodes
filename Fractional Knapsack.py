674f0c45f1b83e04ba0a1f8ab47a0352a28bc7a0
Swanand Kulkarni <93972307+RyomenuSukuna@users.noreply.github.com>
2023-11-07 21:58:29 +0530

    Update Fractional Knapsack.py

--- Fractional Knapsack.py
+++ Fractional Knapsack.py
@@ -1,18 +1,18 @@
 class Solution:
     def solve(self, weights, values, capacity):
         res = 0
-        # Create pairs of (weight, value) and sort them by value-to-weight ratio in descending order.
+        
         sorted_items = sorted(zip(weights, values), key=lambda x: -x[1] / x[0])
 
         for pair in sorted_items:
             if not bool(capacity):
                 break
             if pair[0] > capacity:
-                # If the item can't be fully added, add a fraction of it based on capacity.
+               
                 res += int(pair[1] * (capacity / pair[0]))
                 capacity = 0
             else:
-                # If the entire item can be added, add it and subtract its weight from capacity.
+               
                 res += pair[1]
                 capacity -= pair[0]
 
